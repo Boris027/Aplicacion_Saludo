8b81921c7cc60eb352fdee87b81cb9184a6ee8bf
Boris027 <titankiller027@gmail.com>
2024-09-24 14:04:19 +0200

    Prueba1

---
+++ app/src/main/java/com/example/helloworldverdadero/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.helloworldverdadero
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val button:Button=findViewById<Button>(R.id.botonxd)
+
+        button.setOnClickListener{
+            val name:String=findViewById<EditText>(R.id.textofinal).text.toString()
+            Snackbar.make(button, "Hola $name", Snackbar.LENGTH_SHORT).show()
+        }
+
+    }
+}
